e7d82969981f38fe611c12d6f48c3f83be45b664
Jimena-Moncada <jimenamoncada20@gmail.com>
2024-08-30 19:52:50 -0400

    Probando la version 1.8

---
+++ src/main/java/com/Backend/Inmobiliaria/controller/Rol_MenuController.java
@@ -0,0 +1,25 @@
+package com.Backend.Inmobiliaria.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Backend.Inmobiliaria.Repository.Rol_MenuRepository;
+import com.Backend.Inmobiliaria.model.Rol_Menu;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Rol_MenuController {
+
+	@Autowired
+	public Rol_MenuRepository rmRepo;
+	
+	@GetMapping({"/listaRolMenu"})
+	public List<Rol_Menu> listaRolmenu(){
+		return rmRepo.findAll();
+	}
+	
+}
